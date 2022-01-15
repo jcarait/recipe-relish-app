642e342de708cc802cb549bca68be3a68b80610a
Jonathan F-Carait <jonathan.carait@outlook.com>
2022-01-15 14:44:28 +1100

    working api fetch on console

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var searchInput = document.querySelector("#search-input");
+var formInput = document.querySelector("#form-input");
+var searchQuery = document.querySelector("#search-query");
 var dietRequirement = document.querySelector("#diet");
 var mealType = document.querySelector("#meal-type");
 
@@ -6,31 +7,28 @@ var apiID = "bf7ef27c";
 var apiKey = "f26b311cc0cf67ce4f322e55dca05398";
 
 var formSubmitHandler = function (event) {
-  event.preventDefault();
-  var searchInput = searchInput.value;
-  if (searchInput) {
-    searchAPi(searchInput);
-  }
+    event.preventDefault();
+    var query = searchQuery.value;
+    if (query) {
+        searchAPi(query);
+    }
 };
 
 var searchAPi = function (query) {
-  var apiUrl =
-    "https://api.edamam.com/api/recipes/v2?" +
-    "q=" +
-    query +
-    "&app_key=" +
-    apiKey +
-    "&app_id=" +
-    apiID;
-  console.log(apiUrl);
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data);
-      });
-    }
-  });
+
+    var apiUrl = "https://api.edamam.com/api/recipes/v2?" + "q=" + query + "&app_key=" + apiKey + "&app_id=" + apiID + "&type=public";
+
+    console.log(apiUrl);
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+
+            });
+        }
+    });
 };
 
-searchInput.addEventListener("submit", formSubmitHandler);
+
+formInput.addEventListener("submit", formSubmitHandler);
 // getApiData();

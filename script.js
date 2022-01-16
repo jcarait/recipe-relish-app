4ac7896cdff96840b372b51322f212306285dc25
Carolina Teixeira <rol_teixeira@hotmail.com>
2022-01-16 17:24:04 +1100

    updated js

--- script.js
+++ script.js
@@ -3,23 +3,37 @@ var searchQuery = document.querySelector("#search-query");
 var dietRequirement = document.querySelector("#diet");
 var mealType = document.querySelector("#meal-type");
 var searchResults = document.querySelector("#recipe-results");
+var veganCheckEl = document.querySelector("#vegan");
 
 var apiID = "bf7ef27c";
 var apiKey = "f26b311cc0cf67ce4f322e55dca05398";
 
 //more vars here
+var veganOption = false
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
+    
     var query = searchQuery.value;
+    var vegan 
+    
+    if (veganOption) {
+        vegan = "vegan";       
+}
+console.log(vegan)
+
     if (query) {
         searchAPi(query);
+        console.log("queryOnly")
+    }
+    else if (query && vegan){
+        searchAPi(query, vegan)
     }
 };
 
-var searchAPi = function (query) {
+var searchAPi = function (query, vegan) {
 
-    var apiUrl = "https://api.edamam.com/api/recipes/v2?" + "q=" + query + "&app_key=" + apiKey + "&app_id=" + apiID + "&type=public";
+    var apiUrl = "https://api.edamam.com/api/recipes/v2?" + "q=" + query + "&health=" + vegan + "&app_key=" + apiKey + "&app_id=" + apiID + "&type=public";
     console.log(apiUrl)
 
     fetch(apiUrl, {
@@ -66,4 +80,12 @@ var displayResults = function (label, url, image) {
 
 
 formInput.addEventListener("click", formSubmitHandler);
+veganCheckEl.addEventListener("change", function(){
+
+    if (this.checked) {
+        veganOption = true
+        console.log(veganOption)
+
+    }
+})
 // getApiData();
